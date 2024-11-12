5f3c36898e65b956850efa8bebbf688bb1d0d79f
Le Anh Tuan <83873640+letuantdh93@users.noreply.github.com>
2024-11-12 14:47:49 +0700

    Update saveDataSayDuong.js

--- scripts/saveDataSayDuong.js
+++ scripts/saveDataSayDuong.js
@@ -34,7 +34,7 @@ function addWidget() {
 }
 function createWidget() {
   document.getElementById("confirm").style.display = "none";
-  fetch("http://localhost:3000/add-say-duong-widget", {
+  fetch("http://103.7.40.78:3000/add-say-duong-widget", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -52,7 +52,7 @@ function hideConfirm() {
 
 function removeWidget() {
   alert("Da xoa Widget");
-  fetch(`http://localhost:3000/delete-say-duong-widget/${divId}`, {
+  fetch(`http://103.7.40.78:3000/delete-say-duong-widget/${divId}`, {
     method: "DELETE",
   })
     .then((response) => response.json())
