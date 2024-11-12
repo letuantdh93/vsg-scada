fb3d918ca314192224fd867de4b94bc141b1f725
Le Anh Tuan <83873640+letuantdh93@users.noreply.github.com>
2024-11-12 14:46:56 +0700

    Update saveDataLyTam.js

--- scripts/saveDataLyTam.js
+++ scripts/saveDataLyTam.js
@@ -34,7 +34,7 @@ function addWidget() {
 }
 function createWidget() {
   document.getElementById("confirm").style.display = "none";
-  fetch("http://localhost:3000/add-ly-tam-widget", {
+  fetch("http://103.7.40.78:3000/add-ly-tam-widget", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -52,7 +52,7 @@ function hideConfirm() {
 
 function removeWidget() {
   alert("Da xoa Widget");
-  fetch(`http://localhost:3000/delete-ly-tam-widget/${divId}`, {
+  fetch(`http://103.7.40.78:3000/delete-ly-tam-widget/${divId}`, {
     method: "DELETE",
   })
     .then((response) => response.json())
