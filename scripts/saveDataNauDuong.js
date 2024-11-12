d0ccb1b15a5bb10ef1acf9ffef4478e5a0490f74
Le Anh Tuan <83873640+letuantdh93@users.noreply.github.com>
2024-11-12 14:47:15 +0700

    Update saveDataNauDuong.js

--- scripts/saveDataNauDuong.js
+++ scripts/saveDataNauDuong.js
@@ -34,7 +34,7 @@ function addWidget() {
 }
 function createWidget() {
   document.getElementById("confirm").style.display = "none";
-  fetch("http://localhost:3000/add-nau-duong-widget", {
+  fetch("http://103.7.40.78:3000/add-nau-duong-widget", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -52,7 +52,7 @@ function hideConfirm() {
 
 function removeWidget() {
   alert("Da xoa Widget");
-  fetch(`http://localhost:3000/delete-nau-duong-widget/${divId}`, {
+  fetch(`http://103.7.40.78:3000/delete-nau-duong-widget/${divId}`, {
     method: "DELETE",
   })
     .then((response) => response.json())
